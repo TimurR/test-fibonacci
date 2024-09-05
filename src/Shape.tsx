247fd22404226e02cdefa9d02c3258951626eda8
TimRas <timur.rasulev.82@gmail.com>
2024-09-05 14:24:46 +0100

    fix for img type

--- src/Shape.tsx
+++ src/Shape.tsx
@@ -3,7 +3,7 @@ import { shapeType } from "./types";
 export function Shape({
   shape,
   ...props
-}: { shape: shapeType } & React.HTMLAttributes<HTMLSpanElement>) {
+}: { shape: shapeType } & React.HTMLAttributes<HTMLImageElement>) {
   return (
     <>
       <img className={`size-8`} src={shape.img} title={shape.name} {...props} />
